a44d397dbc06fdaee26c271841bde3faa717ea59
Nibin Benjamin <nibin501@gmail.com>
2023-10-22 10:13:51 +0000

    Update 3 files for codesandbox integration

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,27 @@
-const HtmlWebPackPlugin = require('html-webpack-plugin');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 const htmlPlugin = new HtmlWebPackPlugin({
- template: './src/index.html',
- filename: './index.html'
+  template: "./src/index.html",
+  filename: "./index.html",
 });
 module.exports = {
-mode: 'development',
+  mode: "development",
+  devServer: {
+    allowedHosts: "all",
+  },
   module: {
-    rules: [{
-   test: /\.js$/,
-   exclude: /node_modules/,
-   use: {
-     loader: 'babel-loader'
-   }
- },
-  {
-   test: /\.css$/,
-   use: ['style-loader', 'css-loader']
-  }
-]},
- plugins: [htmlPlugin]
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [htmlPlugin],
 };
