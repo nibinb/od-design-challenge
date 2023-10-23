f98cd7495530258d7a4e47938f39416e2a3cc1f8
nBenjamin <NBenjamin@HollandAmerica.com>
2023-10-23 16:03:06 +0530

    Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const htmlPlugin = new HtmlWebPackPlugin({
 });
 const path = require("path");
 module.exports = {
+  entry: "./src/index.js",
   output: {
     path: path.join(__dirname, "/dist"),
     filename: "bundle.js",
